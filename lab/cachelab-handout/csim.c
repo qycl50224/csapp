0637fad0092b4fd37d207d293afd77e6bcffacb1
qycl50224 <qy50224@163.com>
2020-10-21 06:11:20 -0700

    overturn the former idea, follow the official guide write basic code, remaining load data core code

--- lab/cachelab-handout/csim.c
+++ lab/cachelab-handout/csim.c
@@ -1,6 +1,12 @@
 #include "cachelab.h"
-// author:qycl50224
+#include <getopt.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+// author:qycl50224
+#define m 64
 
 // address = 64 bits, so m = 64, t = m - (b + s)
 // only consider implementation of L and M and S, no I
@@ -10,72 +16,147 @@
 // adderss
 // target:track hit, miss and eviction times 
 
-int main()
-{
-    printSummary(0, 0, 0);
-    return 0;
-}
 
-void read()
-{
-	int s;
-	int E;
-	int b;	
-}
+// read input s,E,b
+// initalize hit, miss, eviction
+// calcS,B,tagBit	
+// malloc C = S * E * (2^(b+tag+1)) byte
+// read instruction 
+//  if (instruction == 'I') jump
+//	else if (instruction == 'L')
+//		read address
+// 		calc idxOfS,t
+// 		for (E raw: S) 
+//			if (isValid && sameTag) print(hit) hit++  update access time flag = 1
+//		if (flag == 0) print(miss) miss++  
+// iterate all row in that set and
+// check valid bit and 
+// check tag bit
+// if (isValid && sameTag) 
 
-int calcSetSize(int s)
+
+int calcS(int s)
 {
 	int res = 1;
 	int i;
-	for (i = 1; i <= s; i++)
-	{
+	for (i = 1; i <= s; i++) {
 		res *= 2;
 	}
 	return res;
 }
 
-int calcBlockSize(int b)
+// int calcB(int b)
+// {
+// 	int res = 1;
+// 	int i;
+// 	for (i = 1; i <= b; i++) {
+// 		res *= 2;
+// 	}
+// 	return res;
+// }
+
+unsigned gett(unsigned long address, int b, int s)
 {
-	int res = 1;
-	int i;
-	for (i = 1; i <= b; i++)
-	{
-		res *= 2;
-	}
-	return res;
+	return address >> (b+s);
 }
 
-int calcTag(int s, int b, int m)
+int getS(unsigned long address, int t, int b)
 {
-	return m-(b+s);
-}
-
-int calct(int setSize, int address)
-{
-	return address / setSize;
-}
-
-int getIndexOfBlock(int address, int blockSize)
-{
-	return address / blockSize;
+	return (((address << t) >> t) >> b) << b;
 }
 	
-int getIndexOfSet(int blockIdx)
+
+int isValid(int v) 
 {
-	return blockIdx % 4;
+	return v? 1:0;
 }
 
-// int getDecimalAddress()
-
-
-Boolean isValid(int v) 
-{
-	return v == 1? true:false;
-}
-
-Boolean sameTag(int realTag, int tag)
+int sameTag(int realTag, int tag)
 {
 	return realTag == tag;
 }
 
+// void loadData(unsigned t, int S, struct cache_line[][] cache)
+// {
+	
+// }
 
+
+struct cache_line {
+	int valid;
+	unsigned tag;
+	time_t time;
+};
+
+int main(int argc, char** argv) 
+{
+	int s,E,opt,S,b,t;
+	char * file = NULL;
+	while(-1 != (opt = getopt(argc, argv, "s:E:b:t:")))
+	{
+		switch(opt)
+		{
+			case 's':
+				s = atoi(optarg);
+				printf("s=%d\n", s);
+				break;
+			case 'E':
+				E = atoi(optarg);
+				printf("E=%d\n", E);
+				break;
+			case 'b':
+				b = atoi(optarg);
+				break;
+			case 't':
+				file = optarg;	
+				printf("file is %s\n", file);
+				break;
+			default:
+				printf("wrong argument\n");
+				break;
+		}
+	}
+
+	S = calcS(s);
+	t = m - (s + b);
+
+
+	struct cache_line cache[S][E];
+	cache[0][0].valid = 1;
+	printf("valid = %d\n", cache[0][0].valid);
+
+	FILE * pFile;
+	pFile = fopen(file, "r");
+
+	char identifier;
+	unsigned long address;
+	int size;
+
+	while(fscanf(pFile, " %c %lx,%d", &identifier, &address, &size)>0)
+	{	
+		int sbits = getS(address, t, b);
+		unsigned tbits = gett(address, b, s);
+		if(identifier == 'L')
+		{
+
+			printf("set = %d\n", sbits);
+			printf("tag bit = %d\n", tbits);
+			// printf("identifier = %c\n", identifier);
+			// printf("address %lx\n", address);
+			// printf("size %d\n", size);
+			// loadData(address);
+		} else if(identifier == 'S')
+		{
+
+			// storeData(address);
+		} else if(identifier == 'M'){
+			// modify(address);
+		} else {
+			continue;
+		}
+	}
+	fclose(pFile);
+
+	printSummary(0, 0, 0);
+    return 0;
+}
