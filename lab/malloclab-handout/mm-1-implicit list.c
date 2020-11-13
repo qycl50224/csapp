d97612db790c0f1a03ed28d4340dd1c59fe8a362
qycl50224 <qy50224@163.com>
2020-11-12 23:44:27 -0800

    add coalescing

--- lab/malloclab-handout/mm-1-implicit list.c
+++ lab/malloclab-handout/mm-1-implicit list.c
@@ -57,7 +57,7 @@ team_t team = {
 #define GET_ALLOC(p) (GET(p) & 0x1)
 
 #define HDRP(bp) ((char *)(bp) - WSIZE)
-#define FTRP(bp) ((char 8)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
 
 #define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE))) 
 #define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
@@ -147,7 +147,41 @@ static void *extend_heap(size_t words)
 	return coalesce(bp);
 }
 
+/*
+ * 
+ */
+static void *coalesce(void *bp)
+{
+	size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+	size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+	size_t size = GET_SIZE(HDRP(bp));
 
+	if (prev_alloc && next_alloc) {  /* case 1 */
+		return bp;
+	}
+
+	else if (prev_alloc && !next_alloc) {  /* case 2 */
+		size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+		PUT(HDRP(bp), PACK(size, 0));
+		PUT(FTRP(bp), PACK(size, 0));
+	}
+
+	else if (!prev_alloc && next_alloc) {   /* case 3 */
+		size += GET_SIZE(FTRP(PREV_BLKP(bp)));
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+		PUT(FTRP(bp), PACK(size, 0));
+		bp = PREV_BLKP(bp);
+	}
+
+	else {
+		size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(HDRP(NEXT_BLKP(bp)));
+		PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+		PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+		bp = PREV_BLKP;
+	}
+
+	return bp;
+}
 
 
 
