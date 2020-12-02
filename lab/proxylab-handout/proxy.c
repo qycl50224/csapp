233c97b30d48a4c7330a3cfb8318ebdbc74493ca
qycl50224 <qy50224@163.com>
2020-12-02 02:32:39 -0800

    add start code

---
+++ lab/proxylab-handout/proxy.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include "csapp.h"
+
+/* Recommended max cache and object sizes */
+#define MAX_CACHE_SIZE 1049000
+#define MAX_OBJECT_SIZE 102400
+
+/* You won't lose style points for including this long line in your code */
+static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
+
+void doit(int fd);
+void read_requesthdrs(rio_t *rp);
+int parse_uri(char *uri, char *filename, char *cgiargs);
+void serve_static(int fd, char *filename, int filesize);
+void get_filetype(char *filename, char *filetype);
+void serve_dynamic(int fd, char *filename, char *cgiargs);
+void clienterror(int fd, char *cause, char *errnum, 
+		 char *shortmsg, char *longmsg);
+
+int main(int argc, char **argv)
+{
+    printf("%s", user_agent_hdr);
+    int listenfd, connfd;
+    char hostname[MAXLINE], port[MAXLINE];
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+
+    /* Check command line args */
+    if (argc != 2) {
+		fprintf(stderr, "usage: %s <port>\n", argv[0]);
+		exit(1);
+    }
+
+    listenfd = Open_listenfd(argv[1]);
+    while (1) {
+		clientlen = sizeof(clientaddr);
+		connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); //line:netp:tiny:accept
+	    Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, 
+	                    port, MAXLINE, 0);
+	    printf("Accepted connection from (%s, %s)\n", hostname, port);
+		// doit(connfd);                                             //line:netp:tiny:doit
+		Close(connfd);                                            //line:netp:tiny:close
+    }
+    
+}
